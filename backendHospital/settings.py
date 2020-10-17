123c0ee7a3bf9bcfcff2f0f678025eddd772d32c
yadavshashwat <y.shashwat@gmail.com>
2020-10-18 01:05:23 +0530

    hospitalMgmt db setup

--- backendHospital/settings.py
+++ backendHospital/settings.py
@@ -74,15 +74,19 @@ WSGI_APPLICATION = 'backendHospital.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hospitaldb',
+        'USER': 'knhuser',
+        'PASSWORD': 'sadasd!@!#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
